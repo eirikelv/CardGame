c582c1b7704e3a70fb6efe573f9445e8ec71c840
Eirik Elvestad <eirik.org@gmail.com>
2022-04-02 19:42:17 +0200

    feat: add javaDock to CardGameController

--- src/main/java/edu/ntnu/idatt2001/cardgame/eirielv/cardgame/CardGameController.java
+++ src/main/java/edu/ntnu/idatt2001/cardgame/eirielv/cardgame/CardGameController.java
@@ -8,6 +8,9 @@ import javafx.scene.image.ImageView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * @author Eirik Elvestad
+ */
 public class CardGameController implements Initializable {
     DeckOfCards deckOfCards = new DeckOfCards();
 
@@ -39,11 +42,15 @@ public class CardGameController implements Initializable {
     @FXML
     private ImageView card5;
 
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
 
+    /**
+     * dealHand deals hands to the dealer table. If there already exist cards on the table, the method clears the table
+     * and deals a new hand of cards
+     */
     @FXML
     public void dealHand() {
         if(deckOfCards.getHandOfCard() == null){
@@ -56,6 +63,10 @@ public class CardGameController implements Initializable {
         showHandsOfCards();
     }
 
+    /**
+     * checkHand gives checkHand button functions. Where the function is to sum the faces on the table, check for flush
+     * check for queen of spades, and show all cards with heart
+     */
     @FXML
     public void checkHand(){
         setSumOfTheFaces();
@@ -64,16 +75,25 @@ public class CardGameController implements Initializable {
         setQueenOfSpades();
     }
 
+    /**
+     * setSumOfTheFaces sets the textfield to hold the sum of all faces
+     */
     @FXML
     public void setSumOfTheFaces(){
         sumOfTheFaces.setText(deckOfCards.sumOfFaces());
     }
 
+    /**
+     * setFlush sets the textfield to have yes or no, depending on if there is a flush or not
+     */
     @FXML
     public void setFlush(){
         flush.setText(deckOfCards.flushCheck());
     }
 
+    /**
+     * setCardsOfHeart sets the testfield to hold all names of the cards with hearts ass String
+     */
     @FXML
     public void setCardsOfHeart(){
         StringBuilder sb = new StringBuilder();
@@ -83,11 +103,19 @@ public class CardGameController implements Initializable {
         cardsOfHeart.setText(sb.toString());
     }
 
+    /**
+     * setQueenOfSpades sets the text to the textfield to say yes or no based on if there exists a queenOfSpades on the
+     * table
+     */
     @FXML
     public void setQueenOfSpades(){
         queenOfSpades.setText(deckOfCards.queenOfSpades());
     }
 
+    /**
+     * showHandsOfCards combines the Strings in the handOfCards arrayList with the images in the resource folder that
+     * holds png's of the cards
+     */
     @FXML
     public void showHandsOfCards(){
         card1.setImage(new Image("/edu/ntnu/idatt2001/cardgame/eirielv/cardgame/ImageOfCards/" + deckOfCards.getHandOfCard().get(0).getAsString() + ".png"));
